aa3f56ea16a2c919b67d61264536aceb7ba22e6e
Edgar Chirivella <hola@edgarchirivella.com>
2022-07-23 17:32:34 +0000

    Add a first integration test

--- specs/integration/server.spec.js
+++ specs/integration/server.spec.js
@@ -1,21 +1,37 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
 const { StatusCodes } = require('http-status-codes');
 
+const { rateLimitService } = require('../../src/services');
 const app = require('../../src/server');
 
 chai.use(chaiHttp);
+chai.use(sinonChai);
 
 const { request, expect } = chai;
 
 describe('POST /take', () => {
-  it('should respond with TOO_MANY_REQUESTS if the quantity of outstanding tokens is 0', async () => {
-    const body = {};
-    const response = await request(app).post('/take').send({
+  const sandbox = sinon.createSandbox();
+
+  afterEach(() => {
+    sinon.restore();
+    sandbox.restore();
+  });
+
+  it('should get ', async () => {
+    const tokens = 5;
+    const stub = sinon.stub(rateLimitService, 'take').returns(tokens);
+
+    const requestBody = {
       method: 'GET',
       endpoint: '/user/:id',
-    });
+    };
+
+    const response = await request(app).post('/take').send(requestBody);
 
     expect(response.status).to.equal(StatusCodes.OK);
+    expect(response.body.tokens).to.equal(tokens);
   });
 });
