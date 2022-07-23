93a2455bfa7fd0541d001926f098e75a176a6365
Edgar Chirivella <hola@edgarchirivella.com>
2022-07-23 14:40:58 +0000

    Create base rate-limit repository

---
+++ src/repositories/rate-limit.repository.js
@@ -0,0 +1,21 @@
+const config = require('./config.json');
+
+class RateLimitRepository {
+    constructor(rateLimitsPerEndpoint) {
+        this.rateLimits = rateLimitsPerEndpoint.reduce((prev, current) => {
+            ...prev,
+            [current.endpoint]: {
+                burst: current.burst,
+                sustained: current.sustained,
+            }
+        }, {});
+    }
+
+    getRateLimit(endpoint) {
+        const limit = this.rateLimits[endpoint] ?? null;
+    }
+}
+
+const rateLimitRepository = new RateLimitRepository(config.rateLimitsPerEndpoint);
+
+module.exports = rateLimitRepository;
