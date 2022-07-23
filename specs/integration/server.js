d1d962df00725ef807c68a705f86e38f3bc6db7e
Edgar Chirivella <hola@edgarchirivella.com>
2022-07-23 16:49:55 +0000

    Separate tests in unit and integration

--- specs/integration/server.js
+++ specs/integration/server.js
@@ -9,16 +9,12 @@ chai.use(chaiHttp);
 const { request, expect } = chai;
 
 describe('POST /take', () => {
-  it('should respond with TOO_MANY_REQUESTS if the quantity of outstanding tokens is 0', aync () => {
-    const body = {
-
-    }
-    const response = await request(app)
-      .post('/take')
-      .send({
-        method: 'GET',
-        endpoint: '/user/:id',
-      });
+  it('should respond with TOO_MANY_REQUESTS if the quantity of outstanding tokens is 0', async () => {
+    const body = {};
+    const response = await request(app).post('/take').send({
+      method: 'GET',
+      endpoint: '/user/:id',
+    });
 
     expect(response.status).to.equal(StatusCodes.OK);
   });
