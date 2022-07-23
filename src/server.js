2b847f9d3b4d72427d44c71509c62918d4a539cc
Edgar Chirivella <hola@edgarchirivella.com>
2022-07-23 18:23:10 +0000

    Install and configure Swagger

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const controller = require('./controller');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 const app = express();
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.post('/take', controller.take);
 
