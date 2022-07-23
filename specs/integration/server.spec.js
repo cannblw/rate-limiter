b9e29cef8df0f00b3a848f62adc332e090f73c60
Edgar Chirivella <hola@edgarchirivella.com>
2022-07-23 17:36:08 +0000

    Add error cases on integration tests

--- specs/integration/server.spec.js
+++ specs/integration/server.spec.js
@@ -34,4 +34,34 @@ describe('POST /take', () => {
     expect(response.status).to.equal(StatusCodes.OK);
     expect(response.body.tokens).to.equal(tokens);
   });
+
+  it('should return 429 HTTP error if the quota has been exceeded', async () => {
+    const tokens = 0;
+    const stub = sinon.stub(rateLimitService, 'take').returns(tokens);
+
+    const requestBody = {
+      method: 'GET',
+      endpoint: '/user/:id',
+    };
+
+    const response = await request(app).post('/take').send(requestBody);
+
+    expect(response.status).to.equal(StatusCodes.TOO_MANY_REQUESTS);
+    expect(response.body.tokens).to.equal(tokens);
+  });
+
+  it('should return 404 HTTP error if the endpoint does not exist', async () => {
+    const tokens = null;
+    const stub = sinon.stub(rateLimitService, 'take').returns(tokens);
+
+    const requestBody = {
+      method: 'GET',
+      endpoint: '/my-fake_end/point',
+    };
+
+    const response = await request(app).post('/take').send(requestBody);
+
+    expect(response.status).to.equal(StatusCodes.NOT_FOUND);
+    expect(response.body.tokens).to.equal(tokens);
+  });
 });
