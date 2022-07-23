1fc4e852661cd7e011599a5a47aff0c80cbec84a
Edgar Chirivella <hola@edgarchirivella.com>
2022-07-23 18:24:36 +0000

    Add new line

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
 const app = express();
+
 app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
